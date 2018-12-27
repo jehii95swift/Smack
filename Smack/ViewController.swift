4bd102b35dff5239f12f1be12ef58f54edce0d5f
Jennifer <jehii95@live.com>
2018-12-27 13:58:59 -0500

    add cocoapods

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Jenifer on 12/27/18.
+//  Copyright © 2018 Jenifer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
