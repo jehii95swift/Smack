2cb42afb6b983f249d34547e62e435f90b5d8841
Jennifer <jehii95@live.com>
2018-12-27 21:02:43 -0500

    fin smack04

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Jenifer on 12/27/18.
+//  Copyright © 2018 Jenifer. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+} 
---
+++ Smack/Utilities/Constans.swift
@@ -0,0 +1,12 @@
+//
+//  Constans.swift
+//  Smack
+//
+//  Created by Jenifer on 12/27/18.
+//  Copyright © 2018 Jenifer. All rights reserved.
+//
+
+import Foundation
+
+//segues
+let TO_LOGIN = "toLogin"
