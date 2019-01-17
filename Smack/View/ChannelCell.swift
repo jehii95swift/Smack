033ab5ff0690dbcb60a5f067c299ab225e960a77
Jennifer <jehii95@live.com>
2019-01-16 19:22:48 -0500

    add channel in the view and select 82

---
+++ Smack/View/ChannelCell.swift
@@ -0,0 +1,34 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by Jenifer on 1/16/19.
+//  Copyright © 2019 Jenifer. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+    
+    //outlets
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+        self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    func configureCell(channel: Channel) {
+        let title = channel.name ?? ""
+        channelName.text = "#\(title)"
+        
+    }
+}
